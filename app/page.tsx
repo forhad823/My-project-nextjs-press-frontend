237ab52a786a35c44dcce3f624d4e1f72c19ebcc
Forhad <forhaduddin823@gmail.com>
2026-08-10 23:48:06 +0600

    added an space

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import Link from "next/link";
-import LikeButton from "./ui/LikeButton";
-
 export default function Home() {
   console.log("root route");
   return (
     <div>
-      <p>Hello NextJS Blogs Page</p>
-      <hr />
-      <Link href={"/blogs/1"}>Blogs</Link>
-      <LikeButton />
+      <h1>Hello, Next js!</h1>
     </div>
   );
 }
